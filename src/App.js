37cd38afeaf6a1966d7374f0ebfcdc47f8de0238
AmirMustefa11 <amirmustefa707@gmail.com>
2022-10-24 00:45:18 +0300

    built front-end for a react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,198 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Navbar";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Form from "react-bootstrap/Form";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Background from "./assets/pic.jpg";
+import ResultCard from "./components/ResultCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [words, setWords] = useState();
+  const [uploaded, setUploaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setUploaded(true);
+    setIsLoading(true);
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+    try {
+      const response = await axios({
+        method: "post",
+        mode: "cors",
+        url: "https://unique-counter.herokuapp.com/count",
+        data: formData,
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => {
+          return response;
+        })
+        .then((data) => {
+          setIsLoading(false);
+          setWords(data.data);
+        });
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error.response);
+      alert("error");
+    }
+  };
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+  useEffect(() => {
+    handleSubmit();
+  }, []);
+
+  if (!uploaded) {
+    return (
+      <div className="App" style={{ minHeight: "100vh" }}>
+        <Header />
+
+        <Row
+          style={{
+            backgroundImage: `url(${Background})`,
+            backgroundSize: "cover",
+            opacity: "1",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div style={{ backgroundColor: "#225470", opacity: "0.81" }}>            
+          
+
+            <Container style={{ height: "95vh" }}>
+            <div style={{display:'flex', flexDirection:"column",justifyContent:"space-between", marginTop:"115px"}}>
+            <div>
+              <h2 style={{ color: "white" }}>
+                Upload your documents ( PDF, docx and Excel only ) to get an
+                instant count of the number of words in your document.
+              </h2>
+            </div>
+            <div>
+              <h3 style={{ color: "white", marginTop:"30px" }}>
+                Please note that we don't store any documents you upload here..
+              </h3>
+            </div>
+           </div>
+              
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "55%",
+                  alignContent: "space-between",
+                }}
+              >
+                <h1 style={{ color: "white" }}>Unique Words counter </h1>
+                <Form onSubmit={handleSubmit}>
+                  <div
+                    style={{
+                      marginBottom: "30px",
+                      fontSize: "1.5rem",
+                      color: "white",
+                    }}
+                  >
+                    Upload .pdf, .docx or .xlsx file{" "}
+                  </div>
+
+                  <div>
+                    <Form.Group name="file" controlId="file" className="mb-3">
+                      <Form.Control
+                        name="file"
+                        type="file"
+                        size="lg"
+                        onChange={handleFileSelect}
+                      />
+                    </Form.Group>
+                  </div>
+
+                  <button
+                    type="submit"
+                    variant="primary"
+                    class="btn btn-outline-light"
+                    value="Upload File"
+                  >
+                    Submit
+                  </button>
+                </Form>
+              </div>
+            </Container>
+          </div>
+        </Row>
+      </div>
+    );
+  }
+
+  return (
+    <div className="App" style={{ minHeight: "100vh" }}>
+      <Header />
+      <Row
+        style={{
+          backgroundImage: `url(${Background})`,
+          backgroundSize: "cover",
+          opacity: "1",
+        }}
+      >
+        <div style={{ backgroundColor: "#225470", opacity: "0.81" }}>
+          <Container
+            style={{
+              backgroundColor: "#225470",
+              height: "95vh",
+              opacity: "0.81",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+                alignContent: "space-between",
+                color: "white",
+              }}
+            >
+              {isLoading ? (
+                <>
+                  <h1 style={{ color: "white" }}>Please Wait </h1>
+                  <div class="spinner-border text-light" role="status">
+                    <span class="sr-only"></span>
+                  </div>
+                </>
+              ) : (
+                words && (
+                  <>
+                    <ResultCard
+                      unique_words={words.unique_words}
+                      total_words={words.total_words}
+                    />
+
+                    <div>
+                      <button
+                        variant="primary"
+                        class="btn btn-outline-light"
+                        value="back  "
+                        onClick={() => {
+                          setUploaded(false);
+                          setWords();
+                        }}
+                      >
+                        sumbit another file
+                      </button>
+                    </div>
+                  </>
+                )
+              )}
+            </div>
+          </Container>
+        </div>
+      </Row>
     </div>
   );
 }
